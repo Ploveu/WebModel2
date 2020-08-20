1a722b978461d2f1ef0c27455fb7eb7b2caa3b34
ploveu <773686795@qq.com>
2020-08-20 17:35:28 +0800

    第一次添加

---
+++ WebModel2/src/main/java/com/guo/webmodel2/Webmodel2Application.java
@@ -0,0 +1,13 @@
+package com.guo.webmodel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webmodel2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webmodel2Application.class, args);
+    }
+
+}
---
+++ WebModel2/src/test/java/com/guo/webmodel2/Webmodel2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guo.webmodel2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webmodel2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
